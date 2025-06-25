5253f6b41380e454a7f28b6ef413c1aa5c40aaa3
tanishq9386 <tanishq9386@gmail.com>
2025-06-25 19:01:14 +0530

    making render compliant pt. 6

--- server.ts
+++ server.ts
@@ -8,12 +8,15 @@ const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
+app.prepare().then(() => {  
   const httpServer = createServer((req, res) => handle(req, res));
   
   initSocket(httpServer);
-
+  httpServer.keepAliveTimeout = 120000;
+  httpServer.headersTimeout = 120000;
   httpServer.listen(port, '0.0.0.0', () => {
     console.log(`> Ready on http://localhost:${port} (and externally)`);
   });
 });
+
+export default {};
